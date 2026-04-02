83eeb17db4ba8a372abf98da3ea1e2541ca4506c
mirellanascimento86 <mirellanascimento86@gmail.com>
2026-04-02 17:44:50 -0300

    Update page.tsx

--- app/(auth)/cadastro/page.tsx
+++ app/(auth)/cadastro/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from 'react'
-import { createClientComponentClient } from '@supabase/ssr'
+import { createClient } from '@/utils/supabase/client'
 import { useRouter } from 'next/navigation'
 
 export default function CadastroPage() {
