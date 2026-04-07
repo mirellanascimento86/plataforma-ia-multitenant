558482cde75f96b83e107a0cf82391e1c28eeb88
mirellanascimento86 <mirellanascimento86@gmail.com>
2026-04-07 11:56:09 -0300

    Update client.ts

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,6 +1,10 @@
-import { createClient } from '@supabase/supabase-js'
+'use client'
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+import { createClient as createSupabaseClient } from '@supabase/supabase-js'
+
+export const createClient = () => {
+  return createSupabaseClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+}
